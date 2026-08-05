13e2b6c12aebf62146c338bc046df3071c9afd30
shagyaero-sudo <shegypumpitup@gmail.com>
2026-08-05 23:47:51 +0000

    reloader222

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Auto-reload otomatis pas ada update versi baru dari Vercel / PWA Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload();
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
