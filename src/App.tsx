2c0db838fd349b31432879c429ae1badee2723b6
shagyaero-sudo <shegypumpitup@gmail.com>
2026-09-06 16:05:04 +0700

    inviginty vision

--- src/App.tsx
+++ src/App.tsx
@@ -127,15 +127,15 @@ export default function App() {
 
   const [showSplash, setShowSplash] = useState<boolean>(true);
 
-  // AUTO SET THEME-COLOR STATUS BAR UNTUK HP (INFINITY VISION)
+  // SET THEME-COLOR TRANSPARENT UNTUK STATUS BAR INFINITY VISION
   useEffect(() => {
     let metaThemeColor = document.querySelector("meta[name='theme-color']");
     if (metaThemeColor) {
-      metaThemeColor.setAttribute('content', '#0e0f12');
+      metaThemeColor.setAttribute('content', 'transparent');
     } else {
       const meta = document.createElement('meta');
       meta.name = 'theme-color';
-      meta.content = '#0e0f12';
+      meta.content = 'transparent';
       document.head.appendChild(meta);
     }
   }, []);
